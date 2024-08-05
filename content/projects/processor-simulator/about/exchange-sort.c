ea6314d3995864a4437c24909ed24d0268644396
Francisco Santana <francisco.santana2002@gmail.com>
2024-08-04 23:16:05 -0500

    Improved documentation + added pseudo-random input

--- content/projects/processor-simulator/about/exchange-sort.c
+++ content/projects/processor-simulator/about/exchange-sort.c
@@ -1,49 +1,136 @@
+#include <malloc.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-uint32_t DATA[] = {9, 4, 1, 2, 6, 4, 9, 1, 4, 1, 10, 15, 7, 14, 12, 9};
-const size_t COUNT = sizeof(DATA) / sizeof(uint32_t);
+void randomize();
+void print_data();
 
-int32_t compare(uint32_t[], uint32_t, uint32_t);
-void swap(uint32_t[], uint32_t, uint32_t);
+typedef uint32_t UWord;
+typedef int32_t Word;
+
+const UWord MIN = 0;
+const UWord MAX = 65535;
+
+UWord *DATA;
+const size_t LEN = 100;
+
+Word compare(UWord[], UWord, UWord);
+void swap(UWord[], UWord, UWord);
 
 int main() {
-    for (int i = 0; i < COUNT; ++i)
-        printf("%u ", DATA[i]);
-    printf("\n");
+    randomize();
+    print_data();
 
-    uint32_t x = 0;
-    uint32_t y;
-    uint32_t min;
+    UWord x = 0;
+    UWord y;
+    UWord min;
 
-    while (x < COUNT) {
+x_loop:
+    while (x < LEN) { // goto end (if x >= LEN)
         min = x;
         y = x + 1;
-        while (y < COUNT) {
-            int32_t comp = compare(DATA, y, min);
+    y_loop:
+        while (y < LEN) { // goto do_swap (if y >= LEN)
+            Word comp = compare(DATA, y, min);
 
             if (comp < 0)
                 min = y;
             y = y + 1;
-        }
+        } // goto y_loop
+    do_swap:
         swap(DATA, x, min);
         x = x + 1;
-    }
+    } // goto x_loop
 
-    for (int i = 0; i < COUNT; ++i)
-        printf("%u ", DATA[i]);
-    printf("\n");
+end:
+    print_data();
+    free(DATA);
 }
 
-int32_t compare(uint32_t array[], uint32_t idx1, uint32_t idx2) {
-    uint32_t left = array[idx1];
-    uint32_t right = array[idx2];
+Word compare(UWord array[], UWord idx1, UWord idx2) {
+    UWord left = array[idx1];
+    UWord right = array[idx2];
 
     return left - right;
 }
 
-void swap(uint32_t array[], uint32_t idx1, uint32_t idx2) {
-    uint32_t temp = array[idx1];
+void swap(UWord array[], UWord idx1, UWord idx2) {
+    UWord temp = array[idx1];
     array[idx1] = array[idx2];
     array[idx2] = temp;
 }
+
+#pragma region diagnostic
+
+void print_data() {
+    for (size_t i = 0; i < LEN; ++i) {
+        printf("%05u ", DATA[i]);
+        if (i % 16 == 15)
+            printf("\n");
+    }
+    printf("\n\n");
+}
+
+UWord X = 0;
+UWord Y = 0;
+UWord bitsnacker_random() {
+    // bitkeeper
+    UWord ob, eb;
+    // bitcounter
+    UWord u = 0, o = 0, e = 0, a = 0;
+    for (size_t i = 0; i < 16; ++i) {
+        UWord be = (X & (i * 2)) != 0;
+        UWord bo = (Y & (i * 2 + 1)) != 0;
+
+        e += be;
+        o += bo;
+        ob = bo << i;
+        eb = be << i;
+        a += ((X ^ Y) & (((o + e) * i) % 32)) != 0;
+        u = bo == be;
+    }
+
+    // diffbits
+    UWord xr = (ob ^ eb) | ((ob & eb) << 16);
+
+    X = (Y << e * u | (((a + e) & o) != u)) + (X >> o);
+    Y = (X * Y * xr) + o * Y + e * X;
+
+    char *_x = (char *)&X;
+    char *_y = (char *)&Y;
+    char tmp = 0;
+
+    // swap outer bytes between X and Y
+    tmp = _x[0];
+    _x[0] = _y[3];
+    _y[3] = tmp;
+    tmp = _x[3];
+    _x[3] = _y[0];
+    _y[0] = tmp;
+
+    // swap inner bytes
+    tmp = _y[1];
+    _y[1] = _y[2];
+    _y[2] = tmp;
+    tmp = _x[1];
+    _x[1] = _x[2];
+    _x[2] = tmp;
+
+    // sum and clamp
+    return ((X + Y) % (MAX - MIN)) + MIN;
+}
+
+void randomize() {
+    srand(time(0));
+    X = rand();
+    Y = rand();
+
+    DATA = calloc(LEN, sizeof(UWord));
+
+    for (size_t i = 0; i < LEN; ++i)
+        DATA[i] = bitsnacker_random();
+}
+
+#pragma endregion
